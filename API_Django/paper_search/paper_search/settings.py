919084e904788947f24eb8d7e8473bf56ee3e55b
ncc02 <cuongtk2002@gmail.com>
2024-05-17 21:44:38 +0700

    fix url anh 1

--- API_Django/paper_search/paper_search/settings.py
+++ API_Django/paper_search/paper_search/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+import os
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- API_Django/paper_search/paper_search/urls.py
+++ API_Django/paper_search/paper_search/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('search', views.search),
     path('trend_year', views.trend_year),
     path('trend_10_year', views.trend_10_year),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
